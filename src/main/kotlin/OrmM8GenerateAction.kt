8e4777375a41ad22ab77a112f2f4db0aea04c23f
Mircea MATEI <matei_tm@yahoo.com>
2019-06-07 23:20:51 +0300

    #1. Adding inputDialogBox

--- src/main/kotlin/OrmM8GenerateAction.kt
+++ src/main/kotlin/OrmM8GenerateAction.kt
@@ -3,10 +3,17 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.notification.NotificationGroup
 import com.intellij.notification.NotificationDisplayType
 import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.openapi.ui.Messages
+
+//import io.flutter
 
 class OrmM8GenerateAction: AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
+        val project = e.getData(PlatformDataKeys.PROJECT)
+        val modelName = Messages.showInputDialog(project, "Enter model name in pascalCase", "New Independent model", Messages.getQuestionIcon())
+        Messages.showMessageDialog(project, "Hello, $modelName!\n I am glad to see you.", "Information", Messages.getInformationIcon())
         val notificationGroup = NotificationGroup("fOrmM8Generator", NotificationDisplayType.BALLOON, true)
         notificationGroup.createNotification("M8 Title",
             "M8 Message",
@@ -15,3 +22,4 @@ class OrmM8GenerateAction: AnAction() {
         ).notify(e.project)
     }
 }
+
